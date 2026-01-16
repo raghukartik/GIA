95dff503f7db5043228cdc15319fd40de6dc1d82
kartik <raghu1ckartik.rk@gmail.com>
2026-01-16 23:00:34 +0530

    getCategoryItems, subcategoryItems, itemPrice endpoints implemented, tax inheritance system implemented by using views

--- backend/controllers/restaurantController.js
+++ backend/controllers/restaurantController.js
@@ -12,7 +12,7 @@ const getRestaurants = async(req, res) => {
             })
         }
 
-        return res.status(201).json({
+        return res.status(200).json({
             success: true,
             results: restaurants.length,
             restaurants
@@ -54,7 +54,7 @@ const getRestaurantsCategories = async(req, res) => {
             })
         }
 
-        return res.status(201).json({
+        return res.status(200).json({
             success: true,
             results: categories.length,
             categories
@@ -69,7 +69,177 @@ const getRestaurantsCategories = async(req, res) => {
     }
 }
 
+const getRestaurantsSubcategories = async(req, res) => {
+    try {
+        const {categoryId} = req.params;
+
+        if(!categoryId){
+            return res.status(400).json({
+                success: "false",
+                message: "categoryId is required"
+            })
+        }
+
+        const queryString = `
+        SELECT *
+        FROM subcategory
+        WHERE category_id = $1
+        `;
+        const restaurantsResult = await pool.query(queryString, [categoryId]);
+        const subCategories = restaurantsResult.rows;
+
+        if(subCategories.length === 0){
+            return res.status(204).json({
+                success: true, 
+                message: "No subcategories found!"
+            })
+        }
+
+        return res.status(200).json({
+            success: true,
+            results: subCategories.length,
+            subCategories
+        })
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        })
+    }
+}
+
+const getCategoryItems = async(req, res) => {
+    try {
+        const {categoryId} = req.params;
+        if(!categoryId){
+            return res.status(204).json({
+                status: false,
+                message: "categoryId required"
+            })
+        }
+
+        const queryString = `Select id, category_id, name, description, is_active, pricing_type, is_bookable from items 
+            where category_id = $1 and is_active = true`
+
+        const result = await pool.query(queryString, [categoryId]);
+        const items = result.rows;
+
+        if(items.length === 0){
+            return res.status(204).json({
+                success: true,
+                message: "No items found"
+            })
+        }
+
+        return res.status(200).json({
+            success: true,
+            results: items.length,
+            items
+        })
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        })
+    }
+}
+
+const getSubcategoryItems = async(req, res) => {
+    try {
+        const {subcategoryId} = req.params;
+        if(!subcategoryId){
+            return res.status(204).json({
+                status: false,
+                message: "subcategoryId required"
+            })
+        }
+
+        const queryString = `Select id, subcategory_id, name, description, is_active, pricing_type, is_bookable from items 
+            where subcategory_id = $1 and is_active = true`
+
+        const result = await pool.query(queryString, [subcategoryId]);
+        const items = result.rows;
+
+        if(items.length === 0){
+            return res.status(204).json({
+                success: true,
+                message: "No items found"
+            })
+        }
+
+        return res.status(200).json({
+            success: true,
+            results: items.length,
+            items
+        })
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        })
+    }
+}
+
+const getItemsPrice = async (req, res) => {
+    try {
+        const { itemId } = req.params;
+
+        if (!itemId) {
+            return res.status(400).json({
+                success: false,
+                message: "itemId required"
+            });
+        }
+
+        const queryString = `
+            SELECT
+                ip.base_price,
+                ip.currency,
+                ip.item_id,
+                t.tax_applicable,
+                t.tax_percentage
+            FROM item_pricing ip
+            JOIN item_effective_tax t ON ip.item_id = t.item_id
+            WHERE ip.item_id = $1
+        `;
+
+        const { rows } = await pool.query(queryString, [itemId]);
+
+        if (rows.length === 0) {
+            return res.status(404).json({
+                success: true,
+                message: "No item pricing found"
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            results: rows.length,
+            itemPrices: rows
+        });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        });
+    }
+};
+
+
+
 export const restaurantController = {
     getRestaurants,
-    getRestaurantsCategories
+    getRestaurantsCategories,
+    getRestaurantsSubcategories,
+    getCategoryItems, 
+    getSubcategoryItems,
+    getItemsPrice
 }
--- backend/routes/restaurantRouter.js
+++ backend/routes/restaurantRouter.js
@@ -3,6 +3,10 @@ import { restaurantController } from "../controllers/restaurantController.js";
 const Router = express.Router();
 
 Router.route('/restaurants').get(restaurantController.getRestaurants);
-Router.route('/restaurants/:restId/categories').get(restaurantController.getRestaurantsCategories)
+Router.route('/restaurants/:restId/categories').get(restaurantController.getRestaurantsCategories);
+Router.route('/category/:categoryId/subcategories').get(restaurantController.getRestaurantsSubcategories);
+Router.route('/category/:categoryId/items').get(restaurantController.getCategoryItems);
+Router.route('/subcategory/:subcategoryId/items').get(restaurantController.getSubcategoryItems);
+Router.route('/items/:itemId/item-price').get(restaurantController.getItemsPrice);
 
 export default Router;
